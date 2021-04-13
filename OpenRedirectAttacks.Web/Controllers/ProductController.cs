d93c84ae5b907fd908f8fd73619888ecb092649c
sedat karasungur <limitless_fearless@hotmail.com>
2021-04-13 11:55:30 +0300

    SqlInjection Bitti

---
+++ OpenRedirectAttacks.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenRedirectAttacks.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(string text)
+        {
+          //  var product = _context.Product.FromSqlRaw("select *from product where Name="+"'"+text+"'").ToList();//böyle bir sorgu sql sorgusu gelirse soruna yol açar
+          //  var product = _context.Product.FromSqlRaw("select *from product where Name={0}",text).ToList();//böyle bir sorgu sql sorgusu gelirse soruna yol açmaz
+          //  var product = _context.Product.FromSqlInterpolated($"select *from product where Name={text}).ToList();//böyle bir sorgu sql sorgusu gelirse soruna yol açmaz
+
+            return View();
+        }
+    }
+}
